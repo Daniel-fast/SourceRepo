396de5ea51ade561a73039efe5cc7af9094470d6
Daniel-fast <69299388+Daniel-fast@users.noreply.github.com>
2022-09-17 10:25:10 -0300

    new realease

---
+++ CodeSnippets/recipe-580809-1.py
@@ -0,0 +1,21 @@
+import fitz                           # import PyMuPDF
+doc = fitz.open("EquacoesNaoLineares.pdf")           # or new: fitz.open(), followed by insertPage()
+page = doc[1]                         # choose some page
+rect = fitz.Rect(50, 100, 300, 400)   # rectangle (left, top, right, bottom) in pixels
+
+# text = """This text will only appear in the rectangle. Depending on width, new lines are generated as required.
+#             \n<- This forced line break will also appear.\t
+#             Now a very long word: abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.
+#             \nIt will be broken into pieces."""
+
+text = "This text will only appear in the rectangle"
+
+
+rc = page.insertTextbox(rect, text, fontsize = 12, # choose fontsize (float)
+                   fontname = "Times-Roman",       # a PDF standard font
+                   fontfile = None,                # could be a file on your system
+                   align = 0)                      # 0 = left, 1 = center, 2 = right
+
+print("unused rectangle height: %g" % rc)          # just demo (should display "44.2")
+
+doc.saveIncr()   # update file. Save to new instead by doc.save("new.pdf",...)
